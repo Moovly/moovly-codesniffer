2d114e73cfc9029121b45da3438dfb9b26e2bb48
rafaello104 <raf.lambelin@gmail.com>
2024-08-08 09:39:55 +0200

    remove concat rule

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,7 +9,6 @@ $config = new PhpCsFixer\Config();
 $config->setRules($ruleSet->getRules());
 $config->setRules([
     '@Symfony' => true,
-    'concat_space' => false,
     'yoda_style' => false,
     'phpdoc_no_package' => false,
     'phpdoc_summary' => false,
